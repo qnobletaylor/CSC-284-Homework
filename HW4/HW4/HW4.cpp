dead836396f871a33b623e0858c9220b8e0317cf
Quinlin <102311011+qnobletaylor@users.noreply.github.com>
2025-04-03 10:44:16 -0400

    added function to test for numeric input from user. Refactored things a bit so there is less repetitious code.

--- HW4/HW4/HW4.cpp
+++ HW4/HW4/HW4.cpp
@@ -2,49 +2,52 @@
 //
 import <iostream>;
 import <string>;
+#include <limits.h>; // for MAX_INT
 import "cpr/cpr.h";
 
 cpr::Response readPost(int postID);
 cpr::Response addPost();
-cpr::Payload getPayload();
+cpr::Payload createPayload();
 cpr::Response updatePost(int postID);
 cpr::Response deletePost(int postID);
+void checkInputError(int& input);
+
+// Global variable for URL, thinking this can be changed in one spot if you wanted to get data from another source
+const std::string BASE_URL{ "https://jsonplaceholder.typicode.com/posts/" };
 
 int main()
 {
 	int choice{ 0 }, postID{ 0 };
 	cpr::Response response{};
 
-
 	do {
 		std::cout << "Please choose an action:\n\t1. Read a Post\n\t2. Add a New Post\n\t3. Update a Post\n\t4. Delete a Post\n\t5. Exit";
 		std::cout << "\nChoice: ";
-		std::cin >> choice;
-
+		checkInputError(choice);
 
 		switch (choice)
 		{
 		case(1):
 			std::cout << "Post ID: ";
-			std::cin >> postID;
+			checkInputError(postID);
 			response = readPost(postID);
-			std::cout << "Fetching ID: " << postID << "\n" << response.text << std::endl;
+			std::cout << "Fetching Post: \n" << response.text << std::endl;
 			break;
 		case(2):
 			response = addPost();
-			std::cout << "Response: \n" << response.text << std::endl;
+			std::cout << "Created Post: \n" << response.text << std::endl;
 			break;
 		case(3):
 			std::cout << "Post ID: ";
-			std::cin >> postID;
+			checkInputError(postID);
 			response = updatePost(postID);
-			std::cout << "Response: \n" << response.text << std::endl;
+			std::cout << "Updated Post: \n" << response.text << std::endl;
 			break;
 		case(4):
 			std::cout << "Post ID: ";
-			std::cin >> postID;
+			checkInputError(postID);
 			response = deletePost(postID);
-			std::cout << response.status_code << " Post deleted." << std::endl;
+			std::cout << response.status_code << " Post deleted successfuly." << std::endl;
 			break;
 		case(5):
 			std::cout << "Exiting program..." << std::endl;
@@ -54,29 +57,25 @@ int main()
 			break;
 		}
 
-
-
 	} while (choice != 5);
 }
 
 cpr::Response readPost(int postID) {
-	cpr::Url url{ "https://jsonplaceholder.typicode.com/posts/" + std::to_string(postID) };
+	cpr::Url url{ BASE_URL + std::to_string(postID) };
 
 	return cpr::Get(url);
 }
 
 cpr::Response addPost() {
-	cpr::Url url{ "https://jsonplaceholder.typicode.com/posts" };
+	cpr::Url url{ BASE_URL };
 
-	return cpr::Post(url, getPayload());
+	return cpr::Post(url, createPayload());
 }
 
-
-cpr::Payload getPayload() {
+cpr::Payload createPayload() {
 	std::string title{}, body{};
 
-	std::cin.ignore(1000, '\n');
-	std::cout << "Please enter a title for post: ";
+	std::cout << "Please enter a title: ";
 	std::getline(std::cin, title);
 	std::cout << "Please enter a body: ";
 	std::getline(std::cin, body);
@@ -85,14 +84,26 @@ cpr::Payload getPayload() {
 }
 
 cpr::Response updatePost(int postID) {
-	cpr::Url url{ "https://jsonplaceholder.typicode.com/posts/" + std::to_string(postID) };
+	cpr::Url url{ BASE_URL + std::to_string(postID) };
 	std::cout << "Updating:" << readPost(postID).text << std::endl;
 
-	return cpr::Put(url, getPayload());
+	return cpr::Put(url, createPayload());
 }
 
 cpr::Response deletePost(int postID) {
-	cpr::Url url{ "https://jsonplaceholder.typicode.com/posts/" + std::to_string(postID) };
+	cpr::Url url{ BASE_URL + std::to_string(postID) };
 
 	return cpr::Delete(url);
+}
+
+void checkInputError(int& input) {
+	std::cin >> std::setw(1) >> input;
+	while (!std::cin.good()) {
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+		std::cout << "Enter a valid integer: ";
+		std::cin >> input;
+	}
+
+	std::cin.ignore(INT_MAX, '\n');
 }
